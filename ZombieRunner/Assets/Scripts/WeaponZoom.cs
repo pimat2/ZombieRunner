37e139c9e554d441074fdb836828faa520fa8f01
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-29 01:53:30 +0200

    Weapon Zooming
    Changing the camera FOV and the Mouse Sensitivity based on whether the player has zoomed in or not(by clicking the right mouse button)

---
+++ ZombieRunner/Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [Header("Camera Zoom")]
+    [SerializeField] float zoomedFOV = 35f;
+    [SerializeField] float normalFOV = 60f;
+    [Header("Mouse Sensitivity")]
+    [SerializeField] float zoomOutSensitivity = 2f;
+    [SerializeField] float zoomInSensitivity = 0.5f;
+    [SerializeField] Camera fpsCamera;
+    RigidbodyFirstPersonController fpsController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1)){
+            ZoomIn();
+        }
+        else{
+           ZoomOut();
+        }
+        
+    }
+    void ZoomIn(){
+        fpsCamera.fieldOfView = zoomedFOV;
+        fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+    }
+    void ZoomOut(){
+        fpsCamera.fieldOfView = normalFOV;
+        fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+    }
+}
