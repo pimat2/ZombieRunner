f723a8603f9deb0574796474f17a559b33628ad9
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-28 20:48:17 +0200

    Made the enemy AI a bit smarter
    Now the enemy will get provoked when certain conditions are met(for the future when the player shoots them). When the enemy starts chasing the player it chases him to a certain point which is indicated by the attack range gizmo onscreen after which it starts executing the attack method which is not implemented yet

--- ZombieRunner/Assets/Scripts/EnemyAI.cs
+++ ZombieRunner/Assets/Scripts/EnemyAI.cs
@@ -6,32 +6,56 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] float attackRange = 3f;
     [SerializeField] float chaseRange = 5f;
     [SerializeField] Transform target;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = attackRange;
     }
 
-    
     void Update()
     {
-        CalculateDistanceToPlayer();
-    }
-    void OnDrawGizmosSelected() {
-        Gizmos.color = new Color(1,0,0);
-        Gizmos.DrawWireSphere(transform.position, chaseRange);    
-    }
-    void CalculateDistanceToPlayer(){
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget <= chaseRange){
-            ChasePlayer();
+        if(isProvoked){
+            EngagePlayer();
+        }
+        else if(distanceToTarget <= chaseRange){
+            isProvoked = true;
         }
     }
-    void ChasePlayer(){
+
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(1,0,0);
+        Gizmos.DrawWireSphere(transform.position, chaseRange); 
+        Gizmos.color = new Color(0,1,0);
+        Gizmos.DrawWireSphere(transform.position, attackRange); 
+    }
+
+
+    void EngagePlayer(){
+        
+        if(distanceToTarget >= navMeshAgent.stoppingDistance){
+            ChaseTarget();
+        }
+        else if(distanceToTarget <= navMeshAgent.stoppingDistance){
+            AttackTarget();
+        }
+    }
+
+
+    void AttackTarget(){
+        Debug.Log("Attacking Player");
+    }
+
+
+    void ChaseTarget(){
         navMeshAgent.SetDestination(target.position);
     }
 }
